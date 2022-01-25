80226cbc43a372f053c7a0c657abe851c1f4edee
U-DESKTOP-7A5237S\Paulo <Paulo Vzquez Acosta>
2022-01-25 11:48:34 +0100

    Creado o proxecto DrogaPOP con hibernate
    Creado o proxecto co Main e importada a dependencia de hibernate con
    Maven

---
+++ DrogaPOP/src/main/java/drogapop/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drogapop;
+
+/**
+ *
+ * @author Paulo
+ * @author Yuda
+ * @author Maikel
+ * @author Nicolás
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
